a5e10ddcc81c8b877cc52e5049a2826bef554247
Rafael Resende <rafael.resende07@gmail.com>
2020-06-24 14:08:37 -0300

    Criação de classe D

---
+++ D.ts
@@ -0,0 +1,28 @@
+class D {
+    private D1: object;
+    private D2: object;
+
+    getD1() {
+        return this.D1;
+    }
+
+    setD1(d1: object) {
+        this.D1 = d1;
+    }
+
+    getD2() {
+        return this.D2;
+    }
+
+    setD2(d2: object) {
+        this.D2 = d2;
+    }
+
+    MD1() {
+        console.log('MD1');
+    }
+
+    MD2() {
+        console.log('MD2');
+    }
+}
